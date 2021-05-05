ef02eace5edd422689237c41a10d0992344992fe
blackgamma7 <blackgamma7@gmail.com>
2021-05-05 00:05:52 -0400

    labeled Bosses in set_boss_flag

--- src_pseudo/bossflag.cpp
+++ src_pseudo/bossflag.cpp
@@ -8,37 +8,37 @@ void set_boss_flag(void){
     if (id == NULL) goto next;
     index = GetIDIndex(id);
     switch(index) {
-    case 0xa8:
+    case 0xa8: //Kitarak
       flg = 0x9a;
       break;
-    case 0xad:
+    case 0xad: //Ehud
       flg = 0xc84;
       break;
-    case 0xac:
+    case 0xac: //Shadow
       flg = 0x1ca;
       break;
-    case 0xaf:
+    case 0xaf: //Ksathra
       flg = 0xe9a;
       break;
-    case 0xb0:
+    case 0xb0: //Shatrevar
       flg = 0xe99;
       break;
-    case 0xb1:
+    case 0xb1: //Mehrdad
       flg = 0xe98;
       break;
-    case 0xb2:
+    case 0xb2: // Golnar
       flg = 0xe97;
       break;
-    case 0xb3:
+    case 0xb3: //Nasim
       flg = 0xe96;
       break;
-    case 0xb4:
+    case 0xb4: //Behrooz
       flg = 0xe95;
       break;
-    case 0xb5:
+    case 0xb5: //Assim
       setEventFlag(0xe9b,true);
       goto next;
-    case 0xba:
+    case 0xba: //Lugash
       flg = 0x215;
       break;
     default:
